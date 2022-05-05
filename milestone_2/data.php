b47ac09732cd149cca3fc83e4dbc527c63de5966
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-05 16:02:26 +0200

    Init

---
+++ milestone_2/data.php
@@ -0,0 +1,9 @@
+<!-- 
+Seconda Milestone:
+Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà,
+attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
+ -->
+
+<?php
+
+?>
