5075fd50fabdb76e3ba2129c99e5c00cbf05eee5
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-05 16:13:30 +0200

    Ho creato l'header

--- milestone_1/index.php
+++ milestone_1/index.php
@@ -16,10 +16,26 @@ al caricamento della pagina ci saranno tutti i dischi.
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+
+    <!-- link per Bootstrap (v5.0) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <script src="script.js" defer></script>
     <title>Vue Dischi php Milestone1</title>
 </head>
 <body>
+
+    <!-- Header con logo -->
+    <header class="container-fluid">
+        
+        <div class="row">
+            <div class="col-2">
+                <img src="img/spotify-logo.png" alt="logo spotify">
+            </div>
+        </div>
+
+    </header>
     
 </body>
 </html>
